212ec14e19434246b62e03d70919349e97770206
ubamongi <ubamongi@gmail.com>
2025-05-24 19:15:04 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,10 @@
+import VocabQuickQuiz from "../VocabQuickQuiz";
 
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Vocabulary Quiz</h1>
+      <VocabQuickQuiz />
+    </main>
+  );
+}
